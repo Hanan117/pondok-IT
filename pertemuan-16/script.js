7e3a80d52f65ce1e2bf01b036d5126d0866813c4
hanan117 <ashrawyhanan1117@gmail.com>
2026-07-28 06:53:10 +0700

    menambahkan tugas script.js dan statement-expression.js

---
+++ pertemuan-16/script.js
@@ -0,0 +1,48 @@
+1. komentar
+
+/* 
+Nama lengkap : Hanan
+Kelas : C
+Tanggal pengerjaan : 27-07-2026
+*/
+
+2. variabel
+
+const nama = "Hanan";
+const umur = 24;
+const sekolah = "Pondok IT";
+const jurusan = "Informasi dan Teknologi";
+
+
+
+3. 
+
+console.log(nama);
+console.log(umur);
+console.log(sekolah);
+console.log(jurusan);   
+
+
+
+4. 
+
+// Menyimpan data diri menggunakan variabel const
+const nama = "Hanan";
+const umur = 24;
+const sekolah = "Pondok IT";
+const jurusan = "Informasi dan Teknologi";
+
+// Menampilkan seluruh data ke console
+console.log(nama);
+console.log(umur);
+console.log(sekolah);
+console.log(jurusan);
+
+
+
+
+5.
+
+
+
+
---
+++ pertemuan-16/statement-expression.js
@@ -0,0 +1,68 @@
+
+
+1. Statement
+
+// Statement: Membuat variabel menggunakan const
+const nilaiA = 15;
+const nilaiB = 5;
+
+// Statement: Menggunakan console.log() untuk mencetak sesuatu
+console.log("=== HASIL EVALUASI EXPRESSION ===");
+
+
+
+
+2. Expression
+
+
+
+const tambah = nilaiA + nilaiB; 
+
+
+const cekBesar = nilaiA > nilaiB; 
+
+
+const teksGabung = "Belajar " + "JavaScript"; 
+
+
+const angkaLiteral = 100; 
+
+
+const teksKapital = "pemrograman web".toUpperCase(); 
+
+
+
+jadi hsilnya akan :
+
+const nilaiA = 10;
+const nilaiB = 5;
+
+const tambah = nilaiA + nilaiB;
+console.log(tambah);
+
+15
+
+
+const cekBesar = nilaiA > nilaiB;
+console.log(cekBesar);
+
+
+true or false
+
+
+const teksGabung = "Belajar " + "JavaScript";
+console.log(teksGabung);
+
+Belajar JavaScript
+
+
+const angkaLiteral = 100;
+console.log(angkaLiteral);
+
+100
+
+const teksKapital = "pemrograman web".toUpperCase();
+console.log(teksKapital);
+
+
+PEMROGRAMAN WEB
